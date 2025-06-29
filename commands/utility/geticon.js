18a992b019a9427e8ba6b358fe2bc8eebd475970
Asheiou <31135263+Asheiou@users.noreply.github.com>
2025-06-29 21:28:57 +0100

    fix geticon, make ping ephem

--- commands/utility/geticon.js
+++ commands/utility/geticon.js
@@ -1,32 +1,29 @@
-import {SlashCommandBuilder} from 'discord.js';
+const {SlashCommandBuilder} = require('discord.js');
 
 module.exports = {
   data: new SlashCommandBuilder()
       .setName('geticon')
       .setDescription('Get an icon from anything!')
-      .addUserOption(option => {
-        option.setName('user')
-            .setDescription('User to get the profile picture from.')
-      }).addStringOption(option => {
-        option.setName('type')
-            .setDescription('Type of icon to get.')
-            .setRequired(true)
-            .addChoices({name: "Server", value: "server"},
-                {name: "Bot", value: "bot"}
-            )
-      }),
-  async execute(interaction) {
-    let user;
-    if ((user = interaction.options.getUser('user')) != null) {
+      .addUserOption(option => option.setName('user')
+          .setDescription('User to get the profile picture from.')
+          .setRequired(false))
+      .addStringOption(option => option.setName('type')
+          .setDescription('Type of icon to get.')
+          .setRequired(false)
+          .addChoices({name: "Server", value: "server"}, {name: "Bot", value: "bot"})), async execute(interaction) {
+    const user = interaction.options.getUser('user');
+    if (user) {
       return await interaction.reply({content: user.displayAvatarURL(), ephemeral: true});
     }
-    let option = interaction.options.getString('type');
+
+    const option = interaction.options.getString('type');
     if (option === 'server') {
       return await interaction.reply({content: interaction.guild.iconURL(), ephemeral: true});
     }
     if (option === 'bot') {
-      return await interaction.reply({content: interaction.bot.displayAvatarURL(), ephemeral: true});
+      return await interaction.reply({content: interaction.client.user.displayAvatarURL(), ephemeral: true});
     }
-    interaction.reply({content: "Subcommand not recognised!", ephemeral: true});
+
+    await interaction.reply({content: "Subcommand not recognised!", ephemeral: true});
   }
 }
